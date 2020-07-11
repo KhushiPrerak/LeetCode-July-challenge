cf7eaacb9d77ff2656b706c7dfe0e75c93a75988
Khushi Prerak <khushiprerak@gmail.com>
2020-07-11 14:54:43 +0530

    Day 10

---
+++ Flatten a Multilevel Doubly Linked List.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(head==NULL) return NULL;
+        Node* cans=NULL;
+        Node* nans=flatten(head->next);
+        if(head->child!=NULL) cans=flatten(head->child);
+        if(cans!=NULL){
+            head->child=NULL; //important step
+            head->next=cans;
+            cans->prev=head;
+            while(cans->next!=NULL){
+                cans=cans->next;
+            }
+            cans->next=nans;
+            if(nans)
+            nans->prev=cans;
+        }
+        else {
+            head->next=nans;
+            if(nans!=NULL)
+            nans->prev=head;
+        }
+        
+        return head;
+        
+    }
+};
